4b06cd667a261682067cb9cfc9fcffcfa75d51b5
Nsoro Allan <allancorp250@gmail.com>
2025-10-13 00:10:48 +0200

    Update project name and title to "Encode / Decode Messages."

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Hide a message in an emoji',
+  title: 'Encode / Decode Messages.',
 }
 
 export default function RootLayout({
